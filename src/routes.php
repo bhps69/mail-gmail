8e7164e78f8e90f6276df5546f4d03fe36fb3bed
bhps69 <33959580+bhps69@users.noreply.github.com>
2019-03-23 17:06:14 +0530

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -5,5 +5,5 @@
  * Date: 3/22/2019
  * Time: 5:32 PM
  */
-Route::get('/getMailsds','Mail\Gmail\GmailController@retrieveMails');
-Route::get('/getMail','Mail\Gmail\mailSearchController@getMail');
+Route::get('/getMailsds','MailList\GmailList\GmailController@retrieveMails');
+Route::get('/getMail','MailList\GmailList\mailSearchController@getMail');
